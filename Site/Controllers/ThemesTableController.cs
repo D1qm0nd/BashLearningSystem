21b03f0a7c27c4dc5efae4e096162a370efc66f4
d1qm0nd <maxikgaming@inbox.ru>
2023-10-28 00:00:17 +0300

    Admin Pages, New Models...

--- Site/Controllers/ThemesTableController.cs
+++ Site/Controllers/ThemesTableController.cs
@@ -11,133 +11,131 @@ using Site.Controllers.Abstract;
 
 namespace Site.Controllers
 {
-    public class AdminsTableController : PermissionNeededController
+    public class ThemesTableController : PermissionNeededController
     {
-        public AdminsTableController(BashLearningContext context, Session<User> session) : base(context, session)
+        public ThemesTableController(BashLearningContext context, Session<User> session) : base(context: context,
+            session: session)
         {
         }
 
-        // GET: AdminsTableController
-        [Route("data/admins")]
+        // GET: ThemesTable
+        [Route("data/themes")]
         public async Task<IActionResult> Index()
         {
             if (!isAdmin()) return KickAction();
-
-            var bashLearningContext = _context.Admins.Include(a => a.User);
-            var view = View(await bashLearningContext.ToListAsync());
-            view.ViewData["isAuthorized"] = _session.Data != null;
-            return view;
+            
+            if (_context.Themes != null)
+            {
+                var view = View(await _context.Themes.ToListAsync());
+                view.ViewData["isAuthorized"] = _session.Data != null;
+                return view;
+            }
+            else return Problem("Entity set 'BashLearningContext.Themes'  is null.");
         }
 
-        // GET: AdminsTableController/Details/5
+        // GET: ThemesTable/Details/5
         public async Task<IActionResult> Details(Guid? id)
         {
             if (!isAdmin()) return KickAction();
 
-            if (id == null || _context.Admins == null)
+            if (id == null || _context.Themes == null)
             {
                 return NotFound();
             }
 
-            var admin = await _context.Admins
-                .Include(a => a.User)
-                .FirstOrDefaultAsync(m => m.AdminId == id);
-            if (admin == null)
+            var theme = await _context.Themes
+                .FirstOrDefaultAsync(m => m.ThemeId == id);
+            if (theme == null)
             {
                 return NotFound();
             }
-
-            var view = View(admin);
+    
+            var view = View(theme);
             view.ViewData["isAuthorized"] = _session.Data != null;
             return view;
         }
 
-        // GET: AdminsTableController/Create
+        // GET: ThemesTable/Create
         public IActionResult Create()
         {
             if (!isAdmin()) return KickAction();
 
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Login");
             var view = View();
             view.ViewData["isAuthorized"] = _session.Data != null;
             return view;
         }
 
-        // POST: AdminsTableController/Create
+        // POST: ThemesTable/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("AdminId,UserId,IsActual")] Admin admin)
+        public async Task<IActionResult> Create([Bind("ThemeId,Name")] Theme theme)
         {
             if (!isAdmin()) return KickAction();
-
-            if (true) //(ModelState.IsValid)
+            
+            if (true)//(ModelState.IsValid)
             {
+                theme.ThemeId = Guid.NewGuid();
                 var time = DateTime.UtcNow;
-                admin.UpdatedUTC = time;
-                admin.CreatedUTC = time;
-                admin.AdminId = Guid.NewGuid();
-                _context.Add(admin);
+                theme.CreatedUTC = time;
+                theme.UpdatedUTC = time;
+                _context.Add(theme);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Login", admin.UserId);
-            
-            var view = View(admin);
+            var view = View(theme);
             view.ViewData["isAuthorized"] = _session.Data != null;
             return view;
         }
 
-        // GET: AdminsTableController/Edit/5
+        // GET: ThemesTable/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
         {
             if (!isAdmin()) return KickAction();
 
-            if (id == null || _context.Admins == null)
+            if (id == null || _context.Themes == null)
             {
                 return NotFound();
             }
 
-            var admin = await _context.Admins.FindAsync(id);
-            if (admin == null)
+            var theme = await _context.Themes.FindAsync(id);
+            if (theme == null)
             {
                 return NotFound();
             }
 
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Login", admin.UserId);
-            var view = View(admin);
+            var view = View(theme);
             view.ViewData["isAuthorized"] = _session.Data != null;
             return view;
         }
 
-        // POST: AdminsTableController/Edit/5
+        // POST: ThemesTable/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id,
-            [Bind("AdminId,UserId,IsActual,CreatedUTC,UpdatedUTC")] Admin admin)
+        public async Task<IActionResult> Edit(Guid id, [Bind("ThemeId,Name")] Theme theme)
         {
             if (!isAdmin()) return KickAction();
 
-            if (id != admin.AdminId)
+            if (id != theme.ThemeId)
             {
                 return NotFound();
             }
 
-            if (true) //(ModelState.IsValid)
+            if (true)//(ModelState.IsValid)
             {
                 try
                 {
-                    admin.UpdatedUTC = DateTime.UtcNow;
-                    _context.Update(admin);
+                    theme.UpdatedUTC = DateTime.UtcNow;
+                    _context.Update(theme);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!AdminExists(admin.AdminId))
+                    if (!ThemeExists(theme.ThemeId))
                     {
                         return NotFound();
                     }
@@ -150,58 +148,58 @@ namespace Site.Controllers
                 return RedirectToAction(nameof(Index));
             }
 
-            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Login", admin.UserId);
-            return View(admin);
-        }
-
-        // GET: AdminsTableController/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
-        {
-            if (!isAdmin()) return KickAction();
-
-            if (id == null || _context.Admins == null)
-            {
-                return NotFound();
-            }
-
-            var admin = await _context.Admins
-                .Include(a => a.User)
-                .FirstOrDefaultAsync(m => m.AdminId == id);
-            if (admin == null)
-            {
-                return NotFound();
-            }
-
-            var view = View(admin);
+            var view = View(theme);
             view.ViewData["isAuthorized"] = _session.Data != null;
             return view;
         }
 
-        // POST: AdminsTableController/Delete/5
+        // GET: ThemesTable/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (!isAdmin()) return KickAction();
+
+            if (id == null || _context.Themes == null)
+            {
+                return NotFound();
+            }
+
+            var theme = await _context.Themes
+                .FirstOrDefaultAsync(m => m.ThemeId == id);
+            if (theme == null)
+            {
+                return NotFound();
+            }
+
+            var view = View(theme);
+            view.ViewData["isAuthorized"] = _session.Data != null;
+            return view;
+        }
+
+        // POST: ThemesTable/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             if (!isAdmin()) return KickAction();
 
-            if (_context.Admins == null)
+            if (_context.Themes == null)
             {
-                return Problem("Entity set 'BashLearningContext.Admins'  is null.");
+                return Problem("Entity set 'BashLearningContext.Themes'  is null.");
             }
 
-            var admin = await _context.Admins.FindAsync(id);
-            if (admin != null)
+            var theme = await _context.Themes.FindAsync(id);
+            if (theme != null)
             {
-                _context.Admins.Remove(admin);
+                _context.Themes.Remove(theme);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool AdminExists(Guid id)
+        private bool ThemeExists(Guid id)
         {
-            return (_context.Admins?.Any(e => e.AdminId == id)).GetValueOrDefault();
+            return (_context.Themes?.Any(e => e.ThemeId == id)).GetValueOrDefault();
         }
     }
 }
